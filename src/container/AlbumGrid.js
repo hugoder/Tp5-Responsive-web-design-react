ff3aa90df5fef84f14c1a13f2f8272195479e6e5
hugoder <hugo.derri@edu.ece.fr>
2019-02-21 22:54:53 +0100

    Album et header ok

--- src/container/AlbumGrid.js
+++ src/container/AlbumGrid.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Album from '../component/Album';
+import { Row, Col} from 'reactstrap'; 
 
 export default class AlbumGrid extends Component {
   render() {
@@ -12,66 +13,88 @@ export default class AlbumGrid extends Component {
           </a>
         </div>
         <section id="albums">
+        <Row>
+        <Col xl="2" lg="3" md="3" sm="6" xs="6">
           <Album
             image="img/album/album_01.png"
             nom="Native (Deluxe)"
             artist="OneRepublic"
             song={17}
           />
+        </Col>
+        <Col xl="2" lg="3" md="3" sm="6" xs="6">
           <Album
             image="img/album/album_02.png"
             nom="Wax Wings"
             artist="Joshua Radin"
             song={11}
           />
+        </Col>
+        <Col xl="2" lg="3" md="3" sm="6" xs="6">
           <Album
             image="img/album/album_03.png"
-            nom="Wax Modern Vampires of th..."
+            nom="Wax Modern Vampires"
             artist="Vampire Weekend"
             song={12}
           />
+        </Col>
+        <Col xl="2" lg="3" md="3" sm="6" xs="6">
           <Album
             image="img/album/album_04.png"
             nom="Night Visions"
             artist="Imagine Dragons"
             song={11}
           />
+        </Col>
+        <Col xl="2" lg="3" md="3" sm="6" xs="6">
           <Album
             image="img/album/album_05.png"
             nom="Trouble Will Find Me"
             artist="The National"
             song={13}
           />
+        </Col>
+        <Col xl="2" lg="3" md="3" sm="6" xs="6">
           <Album
             image="img/album/album_06.png"
             nom="Native (Deluxe)"
             artist="OneRepublic"
             song={17}
           />
+        </Col>
+        <Col xl="2" lg="3" md="3" sm="6" xs="6">
           <Album
             image="img/album/album_07.png"
             nom="Wax Wings"
             artist="Joshua Radin"
             song={11}
           />
+        </Col>
+        <Col xl="2" lg="3" md="3" sm="6" xs="6">
           <Album
             image="img/album/album_08.png"
             nom="Modern Vampires of th..."
             artist="Vampire Weekend"
             song={12}
           />
+        </Col>
+        <Col xl="2" lg="3" md="3" sm="6" xs="6">
           <Album
             image="img/album/album_09.png"
             nom="Night Visions"
             artist="Imagine Dragons"
             song={11}
           />
+        </Col>
+        <Col xl="2" lg="3" md="3" sm="6" xs="6">
           <Album
             image="img/album/album_10.png"
             nom="Trouble Will Find Me"
             artist="The National"
             song={13}
           />
+        </Col>
+        </Row>
         </section>
       </div>
     );
--- src/container/Header.js
+++ src/container/Header.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
 import logo from '../img/logo.png';
+import { Row, Col} from 'reactstrap';
 
 class Header extends Component {
   render() {
     return (
       <header>
         <div class="wrapper">
-          <div class="logo-search">
-            <img class="pull-left" src={logo} alt="logo" />
-          </div>
-
-          <form method="POST" action="index.html">
-            <input type="search" required class="search" />
-            <input
-              type="submit"
-              name="submit"
-              value="Rechercher"
-              class="submit"
-            />
-          </form>
-          <div class="user">
-            <a href="#">Luke Chesser</a>
-          </div>
+          <Row>
+            <Col lg="4">
+              <div class=" logo-search ">
+                <img class="pull-left" src={logo} alt="logo" />
+              </div>
+            </Col>
+            <Col lg="5" className="d-none d-lg-block">
+              <form method="POST" action="index.html">
+                <input type="search" required class=" search" />
+                <input
+                  type="submit"
+                  name="submit"
+                  value="Rechercher"
+                  class=" submit"
+                />
+              </form>
+            </Col>
+            <Col lg="3">
+              <div class="user ">
+                <a href="#" >Luke Chesser</a>
+              </div>
+            </Col>
+          </Row>
         </div>
+
       </header>
     );
   }
